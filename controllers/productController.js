e5a2dd6801f5916713d5e5246e402a9526efcec9
felipesaev <felipesaev@gmail.com>
2020-10-20 09:14:24 -0300

    get single product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,34 @@
 const Product = require('../models/productModel')
 
+//@desc get all products
+//@route  api/products
+
 async function getProducts(request, response) {
-        const products = await Product.findAll()
-        response.writeHeader(200, {'Content-Type': 'aplication/json'});
-        response.end(JSON.stringify(products))
+    const products = await Product.findAll()
+    response.writeHeader(200, {'Content-Type': 'aplication/json'});
+    response.end(JSON.stringify(products))
+}
+
+
+//@desc get single product
+//@route  api/products/:id
+
+async function getProduct(request, response, id) {
+    try {
+        const product = await Product.findById(id)
+        if(!product) {
+            response.writeHeader(404, {'Content-Type': 'aplication/json'});
+            response.end(JSON.stringify({message: 'Product Not Found'}));
+        } else {
+            response.writeHeader(200, {'Content-Type': 'aplication/json'});
+            response.end(JSON.stringify(product));
+        }
+    } catch(error) {
+        console.log(error);
+    }
 }
 
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct
 }
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const http = require('http')
-const { getProducts } = require('./controllers/productController')
+const { getProducts, getProduct } = require('./controllers/productController')
 
-const server = http.createServer((request, response) => {
+const server = http.createServer((request, response, id) => {
     if(request.url === '/api/products' && request.method === 'GET') {
         getProducts(request, response)
+    } else if(request.url.match(/\/api\/products\/([0-9]+)/) && request.method === 'GET') {
+        const id = request.url.split('/')[3]
+        getProduct(request, response, id)        
     } else {
         response.writeHeader(404, {'Content-Type': 'aplication/json'});
         response.end(JSON.stringify({message:'Route Not found'}))
+        console.log('dsda', request.url.match(/\/api\/products\/([0-9]+)/))
     }
 });
 
